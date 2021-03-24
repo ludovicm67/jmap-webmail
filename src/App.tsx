7d7f5d5d6ddcf5e8232f09ef40991078eb159660
Ludovic Muller <ludovicmuller1@gmail.com>
2021-03-24 11:18:32 +0100

    login: create page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Switch } from 'react-router';
+import { Route } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/js/all';
 import MailLayout from './features/mail/Layout';
+import LoginLayout from './features/login/Layout';
+import { FEATURE_URL as MailUrl } from './features/mail/utils';
+import { FEATURE_URL as LoginUrl } from './features/login/utils';
 import './App.css';
 
 function App(): JSX.Element {
@@ -12,7 +17,10 @@ function App(): JSX.Element {
         </div>
       </nav>
       <div className="app-container">
-        <MailLayout />
+        <Switch>
+          <Route path={LoginUrl} component={LoginLayout} />
+          <Route path={MailUrl} component={MailLayout} />
+        </Switch>
       </div>
     </div>
   );
---
+++ src/features/login/Layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Layout.css';
+
+function Layout(): JSX.Element {
+  return (
+    <div className="login-layout">
+      <h1>Login</h1>
+    </div>
+  );
+}
+
+export default Layout;
