118bb2f774d727e2d7ac67b7ba3212aa26932d8a
Ludovic Muller <ludovicmuller1@gmail.com>
2021-07-04 15:39:54 +0200

    upgrade dependencies

--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -36,11 +36,13 @@ export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 // can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
 // will call the thunk with the `dispatch` function as the first argument. Async
 // code can then be executed and other actions can be dispatched
-export const incrementAsync = (amount: number): AppThunk => (dispatch) => {
-  setTimeout(() => {
-    dispatch(incrementByAmount(amount));
-  }, 1000);
-};
+export const incrementAsync =
+  (amount: number): AppThunk =>
+  (dispatch) => {
+    setTimeout(() => {
+      dispatch(incrementByAmount(amount));
+    }, 1000);
+  };
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
