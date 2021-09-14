f2e166a6542fab2d0457765c171a1dc93715a2fc
Ludovic Muller <ludovicmuller1@gmail.com>
2021-09-14 23:19:33 +0200

    mail: use useQuery to load mail content

--- src/features/mail/content/Mail.tsx
+++ src/features/mail/content/Mail.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useQuery } from 'react-query';
 import SanitizedHtml from '../../../components/SanitizedHtml';
 import { fetchMail } from '../../../lib/jmap';
 import { getLoginPayload } from '../../login/loginSlice';
@@ -11,6 +11,14 @@ type MailProps = {
 type MailContent = {
   content: string;
   type: string;
+};
+
+type FetchMailContentArgs = {
+  endpoint: string;
+  identifier: string;
+  mailId: string;
+  authorizationHeader: string;
+  downloadUrl: string;
 };
 
 const replaceDownloadUrl = (
@@ -31,72 +39,90 @@ const replaceDownloadUrl = (
   ).href;
 };
 
+const fetchMailContent = async (
+  args: FetchMailContentArgs,
+): Promise<MailContent | undefined> => {
+  const { endpoint, identifier, mailId, authorizationHeader, downloadUrl } =
+    args;
+  const mailRequest = await fetchMail(endpoint, identifier, mailId, {
+    Authorization: authorizationHeader,
+  });
+
+  if (!mailRequest.success) {
+    return;
+  }
+
+  const mailData = mailRequest.data;
+
+  if (!mailData.htmlBody || mailData.htmlBody.length < 1) {
+    return;
+  }
+
+  const body = mailData.htmlBody[0];
+
+  if (!body.blobId) {
+    return;
+  }
+
+  const blobUrl = replaceDownloadUrl(
+    identifier,
+    body.blobId,
+    'content',
+    `${body.type};charset=${body.charset}`,
+    downloadUrl,
+    endpoint,
+  );
+
+  return fetch(blobUrl, {
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      Authorization: authorizationHeader,
+    }),
+  })
+    .then((r) => r.text())
+    .then((t) => ({
+      content: t,
+      type: body.type,
+    }));
+};
+
 function Mail(props: MailProps): JSX.Element {
   const mailId = props.mailId;
   const loginDetails = useSelector(getLoginPayload);
   const { authorizationHeader, endpoint, identifier, downloadUrl } =
     loginDetails;
-  const [mailContent, setMailContent] = useState<MailContent>({
-    content: 'Loading…',
-    type: 'text/plain',
-  });
 
-  const loadMailContent = async () => {
-    const mailRequest = await fetchMail(endpoint, identifier, mailId, {
-      Authorization: authorizationHeader,
-    });
-
-    if (!mailRequest.success) {
-      return;
-    }
-
-    const mailData = mailRequest.data;
-
-    if (!mailData.htmlBody || mailData.htmlBody.length < 1) {
-      return;
-    }
-
-    const body = mailData.htmlBody[0];
-
-    if (!body.blobId) {
-      return;
-    }
-
-    const blobUrl = replaceDownloadUrl(
-      identifier,
-      body.blobId,
-      'content',
-      `${body.type};charset=${body.charset}`,
-      downloadUrl,
+  const { isLoading, error, data } = useQuery(`mail/${mailId}`, () =>
+    fetchMailContent({
       endpoint,
-    );
+      identifier,
+      mailId,
+      authorizationHeader,
+      downloadUrl,
+    }),
+  );
 
-    fetch(blobUrl, {
-      headers: new Headers({
-        'Content-Type': 'application/json',
-        Authorization: authorizationHeader,
-      }),
-    })
-      .then((r) => r.text())
-      .then((t) => {
-        setMailContent({
-          content: t,
-          type: body.type,
-        });
-      });
-  };
+  if (isLoading) {
+    return <p>Loading…</p>;
+  }
+
+  if (error || !data) {
+    return (
+      <p>
+        An error occured while trying to load the email content… Please try
+        refreshing the page!
+      </p>
+    );
+  }
 
   return (
     <div className="mail-layout-center has-background-white is-flex-direction-column">
-      <div className="has-text-weight-bold">
-        <button onClick={() => loadMailContent()}>Load mail content</button>
-      </div>
       <div className="has-text-left">
-        {(mailContent.type === 'text/plain' && (
+        {(data.type === 'text/plain' && (
           <pre className="p-4 has-background-white">
-            <SanitizedHtml html={mailContent.content} />
+            <SanitizedHtml html={data.content} />
           </pre>
-        )) || <SanitizedHtml html={mailContent.content} />}
+        )) || <SanitizedHtml html={data.content} />}
       </div>
     </div>
   );
