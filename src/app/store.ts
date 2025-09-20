5b18b5a67feebba49fa987e8d049c74ec071e3c1
Ludovic Muller <ludovicmuller1@gmail.com>
2025-09-20 17:30:10 +0200

    chore(deps): upgrade react-redux to v9

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import mailReducer from '../features/mail/mailSlice';
 import loginReducer from '../features/login/loginSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     mail: mailReducer,
     login: loginReducer,
   },
