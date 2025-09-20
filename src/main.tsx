3d1e42cecc0e954aff0139614bdb93e8deda7340
Ludovic Muller <ludovicmuller1@gmail.com>
2025-09-20 17:43:47 +0200

    chore: remove service worker and upgrade to React 19

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
@@ -26,8 +25,3 @@ root.render(
     </QueryClientProvider>
   </React.StrictMode>,
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
